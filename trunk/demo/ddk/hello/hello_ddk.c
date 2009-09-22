82974fef445a6099adcc131605f5421e75a1b118
fox000002 <fox000002@48c0247c-a797-11de-8c46-7bd654735883>
2009-09-22 19:18:40 +0000

    Fix ddk Entry call convection and link flags.

--- trunk/demo/ddk/hello/hello_ddk.c
+++ trunk/demo/ddk/hello/hello_ddk.c
@@ -1,3 +1,28 @@
+/**
+ * Add a serial port COM1 to vmware XP
+ *
+ *
+ * Cmd:
+ *   attrib -h -s -a -r boot.ini
+ *
+ * Add boot option:
+ *   /debug /debugport=com1 /baudrate=115200
+ *
+ * Cmd:
+ *   attrib +h +s +a +r boot.ini
+ *
+ * Install driver with SRVINSTW.EXE
+ *
+ * Host:
+ *   windbg.exe -k com:port=\\.pipe\com_1,baud=115200,pipe
+ *
+ * Cmd:
+ *   net start first
+ *
+ * Cmd:
+ *   net stop first
+ */
+
 // first.c
 #ifdef __GNUC__
   #include <ddk/ntddk.h>
@@ -10,17 +35,17 @@ VOID DDKAPI DriverUnload(PDRIVER_OBJECT driver)
     DbgPrint("first: Our driver is unloading...\r\n");
 }
 
-NTSTATUS DriverEntry(PDRIVER_OBJECT driver,
+NTSTATUS DDKAPI DriverEntry(PDRIVER_OBJECT driver,
 PUNICODE_STRING reg_path)
 {
-#if HU_DEBUG
+#ifdef HU_DEBUG
 
 #ifdef _MSC_VER
     _asm int 3
 #endif // _MSC_VER
 
 #ifdef __GNUC__
-    __asm__ ("int 3");
+    __asm__ ("int $3");
 #endif // __GNUC__
 
 #endif
