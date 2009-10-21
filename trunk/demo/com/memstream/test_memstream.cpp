eea2732dcdbabf28c5dbd85acd0f5fc8b4208cfa
fox000002 <fox000002@48c0247c-a797-11de-8c46-7bd654735883>
2009-10-21 08:40:26 +0000

    Add the prime demo.

--- trunk/demo/com/memstream/test_memstream.cpp
+++ trunk/demo/com/memstream/test_memstream.cpp
@@ -1,26 +1,30 @@
 #include <windows.h>
 #include <tchar.h>
 
+#include "ufile.h"
+#include "umsg.h"
+
 #include "memstream.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpszCmdLine, int nCmdShow)
 {
-    IMemStream strm;
+    {
+        IMemStream strm;
 
-    strm << "This is ";
-    strm << "an example ";
-    strm << "of a multibyte ";
-    strm << "string builder ";
-    strm << "using CStream";
-    // NULL terminate
-    strm << char(0);
+        strm << "This is ";
+        strm << "an example ";
+        strm << "of a multibyte ";
+        strm << "string builder ";
+        strm << "using IMemStream";
+        // NULL terminate
+        strm << char(0);
 
-    // now get the pointer to the stream buffer, casting it to the correct type
+        // now get the pointer to the stream buffer, casting it to the correct type
 
-    PSTR pszString = (PTSTR) strm.GetBuf();
-    
-    ::MessageBoxA( NULL, pszString, "info", MB_OK|MB_ICONINFORMATION );
-    
+        PSTR pszString = (PTSTR) strm.GetBuf();
+        
+        ::MessageBoxA( NULL, pszString, "info", MB_OK|MB_ICONINFORMATION );
+    }
     {
         IMemStream strm;
 
@@ -28,7 +32,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpszCmdLine, int nCmdSh
         strm << L"an example ";
         strm << L"of a Unicode ";
         strm << L"string builder ";
-        strm << L"using CStream";
+        strm << L"using IMemStream";
         // NULL terminate
         strm << WCHAR(0);
 
@@ -36,7 +40,25 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpszCmdLine, int nCmdSh
 
         PWSTR pszString = (PWSTR) strm.GetBuf();
         
-        MessageBoxW( NULL, pszString, L"", 0 );
+        MessageBoxW( NULL, pszString, L"info", MB_OK|MB_ICONINFORMATION  );
+    }
+    {
+        IMemStream strm;
+        char szFilename[300];
+        ::GetModuleFileName( NULL, szFilename, sizeof(szFilename) );
+
+        UFile file;
+        file.open(szFilename);
+
+        BYTE buf[500];
+        DWORD dwRead;
+
+        while( file.read( buf, sizeof(buf), &dwRead) && 0 != dwRead )
+        {
+            strm.Write( buf, dwRead, NULL );
+        }
+        
+        showMsgFormat("info", "Size of this .exe file: %.2f K", strm.GetEndPos()/1024.0);
     }
     return 0;
 }
