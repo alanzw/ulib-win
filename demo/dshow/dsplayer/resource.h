03682cd1417bbdcbbad54db34ec85a0a904fdd89
fox000002 <fox000002@48c0247c-a797-11de-8c46-7bd654735883>
2009-11-08 20:35:55 +0000

    Add the DirectShow demo.

---
+++ demo/dshow/dsplayer/resource.h
@@ -0,0 +1,17 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDC_EXIT                        3
+#define IDC_OPENFILE                    4
+#define IDD_DIALOG1                     101
+#define IDD_DLGDSPLAYER                 101
+#define IDI_ICONLARGE                   102
+#define IDC_PLAYPAUSE                   1001
+#define IDC_STOP                        1002
+#define IDC_NOWPLAYING                  1003
+#define IDC_TIMEELAPSED                 1004
+#define IDC_TOTALTIME                   1005
+#define IDC_SEPARATOR                   1006
+#define IDC_PROGRESSBAR                 1008
+
