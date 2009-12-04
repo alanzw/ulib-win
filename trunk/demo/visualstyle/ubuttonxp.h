70699b4004c00e0a8577fb935fa51da23e8f7d4c
fox000002 <fox000002@48c0247c-a797-11de-8c46-7bd654735883>
2009-12-04 15:09:45 +0000

    Update the visual style code.

--- trunk/demo/visualstyle/ubuttonxp.h
+++ trunk/demo/visualstyle/ubuttonxp.h
@@ -6,7 +6,14 @@
 class UButtonXP : public UOwnerDrawnButton
 {
 public:
-    UButtonXP(HWND hParent, UINT nResource, HINSTANCE hInst);
+    enum ICON_ALIGNMENT
+    {
+        CENTER = 0,
+        LEFT,
+        RIGHT
+    };
+public:
+    UButtonXP(HWND hParent, UINT nResource, HINSTANCE hInst)
     : UOwnerDrawnButton(hParent, nResource, hInst)
     {}
 
@@ -23,11 +30,20 @@ public:
 
         RECT rc = lpDrawItem->rcItem;
         UINT state = lpDrawItem->itemState;
+        
+        //
+        drawText(hdc, &rc);
 
         return TRUE;
     }
 
-
+private:
+    void drawText(HDC hdc, LPRECT lpRect)
+    {
+        TCHAR sCaption[512];
+        this->getWindowText(sCaption, 512);
+        ::DrawText(hdc, sCaption, lstrlen(sCaption), lpRect, DT_SINGLELINE | DT_CENTER | DT_VCENTER );
+    }
 };
 
 #endif // U_BUTTON_XP_H
