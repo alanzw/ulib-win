8f1089884d9fe46810987cd188d2c9c5216fe5af
fox000002 <fox000002@48c0247c-a797-11de-8c46-7bd654735883>
2010-01-07 18:12:39 +0000

    Update the ODBC demo.

--- inc/uodbc.h
+++ inc/uodbc.h
@@ -6,16 +6,20 @@
 namespace UODBC
 {
 
-class DataBase
+class ULIB_API DataBase
 {
 public:
     DataBase();
     ~DataBase();
     SQLRETURN connect(char *dsnName, char *userId, char *passwd);
+    SQLRETURN connect(const char *filename);
     void disconnect();
     bool exec(const char *stmt);
     bool getdata(char *buf, SQLINTEGER *cbData);
     SQLLEN getRows();
+    
+    //
+    SQLRETURN setLoginTimeout(int nSec);
     
     //
     void extract_error(char *fn);
