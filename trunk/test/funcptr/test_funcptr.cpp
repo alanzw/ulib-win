660103b6406588f46be7f0bc4bd3064d532600bc
fox000002 <fox000002@48c0247c-a797-11de-8c46-7bd654735883>
2010-01-08 04:36:18 +0000

    Add the OLEDB demo.

--- trunk/test/funcptr/test_funcptr.cpp
+++ trunk/test/funcptr/test_funcptr.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 #include "adt/ufuncptr.h"
 
@@ -14,10 +15,62 @@ FuncPtr0_ f()
     return f;
 } // natural return syntax
 
+template <typename T>
+class UComPtr
+{
+public:
+    typedef T Type;
+    typedef T* TypePtr;
+public:
+    UComPtr()
+    : _p(0)
+    {
+    }
+
+    UComPtr(T * p)
+    {
+        if (0 != (_p=p))
+        {
+            //_p->AddRef();
+        }
+    }
+
+    ~UComPtr()
+    {
+        if(_p)
+        {
+            //_p->Release();
+            delete _p;
+        }
+    }
+
+    
+    operator T*() const
+    {
+        return (T*)_p;
+    }
+    
+    T& operator*() const
+    {
+        return *_p;
+    }
+    
+    T * operator->() const
+    {
+        assert(_p);
+        return (T*)_p;
+    }
+private:
+    TypePtr _p;
+};
+
+struct Object
+{
+    int x;
+};
+
 int main()
 {
-
-
     cout << "=========================" << endl;
     FuncPtr0 p = f();  // natural usage syntax
     p();
@@ -25,6 +78,10 @@ int main()
     huys::ADT::Func0<FuncPtr0_>::type pp = f();
     pp();
 
+    UComPtr<Object> px(new Object);
+    
+    cout << px->x << endl;
+    
     return 0;
 }
 
