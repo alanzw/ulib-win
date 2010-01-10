b27944f264e4156924ebcc67f6183500ca6e7bef
fox000002 <fox000002@48c0247c-a797-11de-8c46-7bd654735883>
2010-01-10 04:38:32 +0000

    Update the combobox demo.

--- demo/combobox/test_combobox.cpp
+++ demo/combobox/test_combobox.cpp
@@ -7,23 +7,27 @@
 #include "udialogx.h"
 #include "ucombobox.h"
 #include "udlgapp.h"
+#include "ustatic.h"
 
 using huys::UDialogBox;
 
 class UDialogExt : public UDialogBox
 {   
     enum {
-        ID_UCOMBOBOX = 11
+        ID_UCOMBOBOX = 11,
+        ID_USTATIC   = 12
     };
 public:
     UDialogExt(HINSTANCE hInst, UINT nID)
     : UDialogBox(hInst, nID),
-      m_pCombo(0)
+      m_pCombo(0),
+      m_pStatic(0)
     {}
 
     ~UDialogExt()
     {
         CHECK_PTR(m_pCombo);
+        CHECK_PTR(m_pStatic);
     }
 
     BOOL onInit()
@@ -35,11 +39,45 @@ public:
         m_pCombo->addText("hello");
         m_pCombo->addText("hello2");
         m_pCombo->setCurSel(0);
+        
+        m_pStatic = new UStatic(m_hDlg, ID_USTATIC, m_hInst);
+        m_pStatic->setPos(20, 80, 120, 30);
+        m_pStatic->setStyles(SS_SUNKEN);
+        m_pStatic->create();
+        
+        TCHAR buf[256];
+        m_pCombo->getText(buf);
+        m_pStatic->setWindowText(buf);
+        
         return TRUE;
+    }
+    
+    BOOL onCommand(WPARAM wParam, LPARAM lParam)
+    {
+        switch ( HIWORD(wParam) )
+        {
+        case CBN_SELCHANGE:
+            {
+                //if ((HWND)lParam == m_pCombo->getHWND())
+                {
+                    TCHAR buf[256];
+                    int nSel = m_pCombo->getCurSel();
+                    m_pCombo->getLBText(nSel, buf);
+                    //m_pStatic->setWindowText("");
+                    //m_pStatic->invalidate();
+                    m_pStatic->setWindowText(buf);
+                    m_pStatic->hide();
+                    m_pStatic->show();
+                }
+                return TRUE;
+            }
+        }
+        return UDialogBox::onCommand(wParam, lParam);
     }
 
 private:
     UComboBox *m_pCombo;
+    UStatic *m_pStatic;
 };
 
 
