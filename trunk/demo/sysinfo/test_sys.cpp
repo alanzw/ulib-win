b69860fd77345dbb775ff593a0786f9341ebd5a3
fox000002 <fox000002@48c0247c-a797-11de-8c46-7bd654735883>
2010-01-22 03:13:57 +0000

    Add the NT security sample.

--- trunk/demo/sysinfo/test_sys.cpp
+++ trunk/demo/sysinfo/test_sys.cpp
@@ -28,26 +28,26 @@ int main(int argc, char *argv[])
 
     //
     cout << "Computer Name: " << usys.getComputerName() << endl;
-    
-   SYSTEM_INFO siSysInfo;
- 
-   // Copy the hardware information to the SYSTEM_INFO structure. 
- 
-   GetSystemInfo(&siSysInfo); 
- 
-   // Display the contents of the SYSTEM_INFO structure. 
 
-    printf("Hardware information: \n");  
+   SYSTEM_INFO siSysInfo;
+
+   // Copy the hardware information to the SYSTEM_INFO structure.
+
+   GetSystemInfo(&siSysInfo);
+
+   // Display the contents of the SYSTEM_INFO structure.
+
+    printf("Hardware information: \n");
     printf("  OEM ID: %u\n", siSysInfo.dwOemId);
-    printf("  Number of processors: %u\n", 
-    siSysInfo.dwNumberOfProcessors); 
-    printf("  Page size: %u\n", siSysInfo.dwPageSize); 
-    printf("  Processor type: %u\n", siSysInfo.dwProcessorType); 
-    printf("  Minimum application address: %lx\n", 
-    siSysInfo.lpMinimumApplicationAddress); 
-    printf("  Maximum application address: %lx\n", 
-    siSysInfo.lpMaximumApplicationAddress); 
-    printf("  Active processor mask: %u\n", 
+    printf("  Number of processors: %u\n",
+    siSysInfo.dwNumberOfProcessors);
+    printf("  Page size: %u\n", siSysInfo.dwPageSize);
+    printf("  Processor type: %u\n", siSysInfo.dwProcessorType);
+    printf("  Minimum application address: %lx\n",
+    siSysInfo.lpMinimumApplicationAddress);
+    printf("  Maximum application address: %lx\n",
+    siSysInfo.lpMaximumApplicationAddress);
+    printf("  Active processor mask: %u\n",
     siSysInfo.dwActiveProcessorMask);
     //
     cout << "Press any key to exit..." << endl;
