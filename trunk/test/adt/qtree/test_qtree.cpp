215bc8425cf303578fcd4fcdba0165541b0d3ad8
fox000002 <fox000002@48c0247c-a797-11de-8c46-7bd654735883>
2010-03-21 07:02:50 +0000

    Add tga_glwin, etc.

---
+++ trunk/test/adt/qtree/test_qtree.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+template <class T>
+class QuadTree
+{
+
+};
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+    using std::cin;
+
+    cin.ignore();
+
+    return 0;
+}
