2ef12858586465c39d54b10c591dcda30cc242d0
fox000002 <fox000002@48c0247c-a797-11de-8c46-7bd654735883>
2010-03-21 07:02:50 +0000

    Add tga_glwin, etc.

---
+++ test/adt/otree/test_otree.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+template <class T>
+class OcTree
+{
+
+};
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+    using std::cin;
+
+    cin.ignore();
+
+    return 0;
+}
