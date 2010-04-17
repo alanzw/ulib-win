47f366a2ab0818a090dc44fb7bcf22e0cb0341fe
fox000002 <fox000002@48c0247c-a797-11de-8c46-7bd654735883>
2010-04-17 07:01:15 +0000

    Update the visualstyle demo.

--- src/uvisualstyle.cpp
+++ src/uvisualstyle.cpp
@@ -19,10 +19,10 @@
  
 #include <windows.h>
 #include <tchar.h>
- 
-#include "ulib.h"
 
 #include "uvisualstyle.h"
+
+#include "adt/ustring.h"
 
 namespace VisualStyles
 {
@@ -64,9 +64,51 @@ BOOL UTheme::enable(BOOL fEnable)
     return S_OK == ::EnableTheming(fEnable);
 }
 
-BOOL UTheme::drawBackgroud(HDC hdc, int iPartId, int iStateId, const RECT *pRect, const RECT *pClipRect)
+BOOL UTheme::drawBackgroud(HDC hdc, int iPartId, int iStateId, const RECT *lpRect, const RECT *lpClipRect)
 {
-    return S_OK == ::DrawThemeBackground(m_hTheme, hdc, iPartId, iStateId, pRect, pClipRect);
+    return S_OK == ::DrawThemeBackground(m_hTheme, hdc, iPartId, iStateId, lpRect, lpClipRect);
+}
+
+BOOL UTheme::drawEdge( HDC dc, LPRECT lpRect, UINT uEdge /*= EDGE_BUMP*/, UINT uFlags /*= BF_RECT*/, int iPartID /*= BP_PUSHBUTTON*/, int iStateID /*= PBS_HOT*/, LPRECT lpClipRect /*= NULL*/ )
+{
+    return S_OK == ::DrawThemeEdge(m_hTheme, dc, iPartID, iStateID, lpRect, uEdge, uFlags, lpClipRect);    
+}
+
+BOOL UTheme::getAppearance() const
+{
+    // For XP - Detect if the Window Style is Classic or XP
+    OSVERSIONINFO osvi;
+    osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+    
+    GetVersionEx(&osvi);
+    
+    if (osvi.dwMajorVersion < 5)	// Earlier than XP
+        return FALSE;
+    
+    /////////////////////////////////////////////////////
+    
+    HKEY	hKey;
+    
+    LPCTSTR szSubKey = _T("Control Panel\\Appearance");
+    LPCTSTR	szCurrent = _T("Current");
+    unsigned char szBuffer[512];
+    DWORD dwSize = 512;
+    
+    if (::RegOpenKeyEx(HKEY_CURRENT_USER, szSubKey, 0L, KEY_READ, &hKey) != ERROR_SUCCESS)
+    {
+        // Can't find it    
+        return FALSE;
+    }
+    
+    ::RegQueryValueEx(hKey, szCurrent, NULL, NULL, szBuffer, &dwSize); 
+    
+    ::RegCloseKey(hKey);
+    
+    
+    if (lstrcmp(_T("Windows Standard"), (LPCTSTR)szBuffer) ==0)
+        return FALSE;
+    
+    return TRUE;
 }
 
 }; // namespace VisualStyles
